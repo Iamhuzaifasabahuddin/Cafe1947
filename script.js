4b10f4cfcdad4b6f57cfa513134f3bb28ad43175
Hexz <huzaifasabah@gmail.com>
2025-10-23 12:37:20 -0400

    Initial Website Layout & Design

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const links = document.querySelectorAll('.animate-link');
+
+links.forEach(link => {
+link.addEventListener('click', e => {
+  e.preventDefault();
+  const target = document.querySelector(link.getAttribute('href'));
+  target.scrollIntoView({ behavior: 'smooth' });
+  target.style.transition = 'opacity 0.8s ease';
+  target.style.opacity = 0;
+  setTimeout(() => target.style.opacity = 1, 200);
+});
+});
